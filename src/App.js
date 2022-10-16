0f25a9d426848476a0baf6a0eb98c542e5388ede
Ryan <ryanwfortin@gmail.com>
2022-10-16 12:08:52 -0700

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import DraftForm from './components/DraftForm';
+import data from './data/fakeData';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Mocks</div>
+      <DraftForm data={data} />
     </div>
   );
 }
