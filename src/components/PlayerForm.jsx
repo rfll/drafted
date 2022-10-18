be50d185dd00c88cefc4c6a24272f8f62635cb6b
Ryan <ryanwfortin@gmail.com>
2022-10-18 15:36:18 -0700

    add alt to img tag

--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -4,7 +4,7 @@ export default function PlayerForm(props) {
 
   return (
     <div className='draft-results'>
-      <div className='player-info' key={props.name}><img className='player-image' src={props.image}></img> {props.name} {props.team}</div>
+      <div className='player-info' key={props.name}><img className='player-image' src={props.image} alt=''></img> {props.name} {props.team}</div>
     </div>
   )
 }
