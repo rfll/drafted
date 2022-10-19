2a0f46da2b7dada69402bd5a32036138e98452fd
Ryan <ryanwfortin@gmail.com>
2022-10-18 17:07:51 -0700

    add liveSearch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import PlayerResults from './components/PlayerResults';
 import data from './data/fakeData';
 import './App.css';
+import LiveSearch from './components/LiveSearch';
 
 function App() {
   return (
     <div>
       <div>Mocks</div>
+      <LiveSearch />
       <PlayerResults data={data} />
     </div>
   );
