6b64b530622b0261d8c5125b374de00c9e1f2265
Ryan <ryanwfortin@gmail.com>
2022-11-03 00:22:02 -0500

    remove console logs

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -9,11 +9,11 @@ export default function Results(props) {
   const results = props.results;
   // const term = props.term;
 
-  console.log(results);
+  // console.log(results);
   // console.log(term);
 
   return results.map(player => {
-    // console.log(player);
+    // console.log(player.name);
     return <PlayerForm key={player.name} {...player} />;
   });
 }
