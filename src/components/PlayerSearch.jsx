2361101be6b446c51127b51b9c08722ad8dcbdea
Ryan <ryanwfortin@gmail.com>
2022-11-16 23:53:31 -0800

    remove onclick

--- src/components/PlayerSearch.jsx
+++ src/components/PlayerSearch.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PlayerSearch(props) {
 
   return (
-    <div className='draft-results' onClick={e => props.onClick(e.target.value)}>
+    <div className='draft-results'>
       <div className='player-info' key={props.name}><img className='player-image' src={props.image} alt='draftee'></img> {props.name} {props.team}</div>
     </div>
   )
