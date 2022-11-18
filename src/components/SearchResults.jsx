8c6acd1fd7a46ae934705c5ef8df01e6bbb7ce59
Ryan <ryanwfortin@gmail.com>
2022-11-17 16:35:20 -0800

    use context and remove props

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,23 +1,25 @@
-// import PlayerSearch from "./PlayerSearch"
+import { useContext } from "react";
+import { draftContext } from "../providers/DraftProvider";
 import PlayerForm from "./PlayerForm";
 import DraftForm from "./DraftForm";
 
 
 
 export default function SearchResults(props) {
-  const results = props.results;
+  const { searchResults, setSearchResults, setSearchTerm, onClick } = useContext(draftContext);
+  // const results = props.results;
   // const draftOrderArray = props.storeData;
 
-  function onClick(e, player) {
-    props.storeData.push(player)
+  // function onClick(e, player) {
+  //   props.storeData.push(player)
 
-    console.log(props.storeData);
+  //   console.log(props.storeData);
 
-    props.setTerm("");
-    props.setResults([]);
-  }
+  //   setSearchTerm("");
+  //   setSearchResults([]);
+  // }
 
-  return results.map(player => {
+  return searchResults.map(player => {
     return <PlayerForm key={player.name} {...player}
       onClick={onClick}
     />;
