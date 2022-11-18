c057c45ac64c304da90d42dd4d1d079e1e82d623
Ryan <ryanwfortin@gmail.com>
2022-11-17 16:32:49 -0800

    add drafted players container

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import LiveSearch from './components/LiveSearch';
 import DraftForm from './components/DraftForm';
 import storeData from './data/storeData';
+import DraftProvider from './providers/DraftProvider';
 
 function App() {
   return (
     <div>
       <div>Mocks</div>
-      <LiveSearch 
-      // data={fakeData}
-      />
-      <DraftForm 
-        storeData={storeData}
-      />
+      <DraftProvider>
+        <LiveSearch
+        // data={fakeData}
+        />
+        <div className='selected-players'>
+        <DraftForm
+          storeData={storeData}
+        /></div>
+      </DraftProvider>
     </div>
   );
 }
