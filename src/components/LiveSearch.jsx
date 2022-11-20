2bac14f9e4d40e9eab279d228e70226e15ecd0d4
Ryan <ryanwfortin@gmail.com>
2022-11-20 14:37:51 -0800

    remove comment

--- src/components/LiveSearch.jsx
+++ src/components/LiveSearch.jsx
@@ -9,41 +9,15 @@ import { draftContext } from "../providers/DraftProvider";
 export default function LiveSearch(props) {
   const { searchResults, searchTerm } = useContext(draftContext);
 
-  // const [term, setTerm] = useState("");
-  // const [results, setResults] = useState([]);
-
-  // const terms = useDebounce(term, 400);
-
-  // useEffect(() => {
-  //   if (!terms) {
-  //     return setResults([])
-  //   }
-
-  //   function loadData() {
-  //     return setResults([...fakeData.filter((c) => c.name.toLowerCase().includes(term.toLowerCase()))]);
-  //   }
-
-  //   loadData();
-
-  // }, [terms]);
-
   return (
     <Fragment>
       <header className="logo">
         {/* <img src="images/brand.png" alt="Brand" /> */}
       </header>
       <main>
-        <SearchBar
-          // setTerm={setTerm}
-        />
+        <SearchBar />
         <SearchResults
-          // key={results.name}
-          // results={results}
-          // setResults={setResults}
-          storeData={storeData}
-          // term={term}
-          // setTerm={setTerm}
-          // terms={terms}
+          // storeData={storeData}
         />
       </main>
     </Fragment>
