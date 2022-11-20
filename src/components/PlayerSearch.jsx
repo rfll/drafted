937bb8993b68d8e510447f6d135e0b1e7aef55df
Ryan <ryanwfortin@gmail.com>
2022-11-20 14:38:03 -0800

    adjust classname

--- src/components/PlayerSearch.jsx
+++ src/components/PlayerSearch.jsx
@@ -3,11 +3,14 @@ import React from "react";
 export default function PlayerSearch(props) {
 
   return (
-    <div className='draft-results'>
+    <div className='drafted-player-container'>
       <div className='selected-player-info' key={props.name}>
-        <img className='selected-player-image' src={props.image} alt='draftee'></img>
+        <div className="draft-position">{props.index}</div>
+        <div className="selected-player-text">
         <div className='selected-player-name'>{props.name}</div> 
         <div className='selected-player-team'>{props.team}</div>
+        </div>
+        <img className='selected-player-image' src={props.image} alt='draftee'></img>
         </div>
     </div>
   )
