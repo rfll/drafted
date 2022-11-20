69a284bf5c456f4e69880a4883ec8dc99dc71aed
Ryan <ryanwfortin@gmail.com>
2022-11-20 14:38:10 -0800

    remove comments

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,24 +1,11 @@
 import { useContext } from "react";
 import { draftContext } from "../providers/DraftProvider";
 import PlayerForm from "./PlayerForm";
-import DraftForm from "./DraftForm";
-
 
 
 export default function SearchResults(props) {
-  const { searchResults, setSearchResults, setSearchTerm, onClick } = useContext(draftContext);
-  // const results = props.results;
-  // const draftOrderArray = props.storeData;
-
-  // function onClick(e, player) {
-  //   props.storeData.push(player)
-
-  //   console.log(props.storeData);
-
-  //   setSearchTerm("");
-  //   setSearchResults([]);
-  // }
-
+  const { searchResults, onClick } = useContext(draftContext);
+  
   return searchResults.map(player => {
     return <PlayerForm key={player.name} {...player}
       onClick={onClick}
