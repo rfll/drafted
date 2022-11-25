1915669306e54148eaee4af2a1d894c9c5803590
Ryan <ryanwfortin@gmail.com>
2022-11-24 22:11:19 -0800

    add state

--- src/providers/DraftProvider.jsx
+++ src/providers/DraftProvider.jsx
@@ -9,7 +9,9 @@ export const draftContext = createContext();
 export default function DraftProvider(props) {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [selectedPlayer, setSelectedPlayer] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState(storeData);
+  const [updatedPlayer, setUpdatedPlayer] = useState();
+  const [draftPosition, setDraftPosition] = useState();
 
   const debounceTerm = useDebounce(searchTerm, 400);
 
@@ -19,7 +21,7 @@ export default function DraftProvider(props) {
     }
 
     function loadData() {
-      return setSearchResults([...fakeData.filter((c) => c.name.toLowerCase().includes(searchTerm.toLowerCase()))]);
+      return setSearchResults([...fakeData.filter((c) => c.name.toLowerCase().includes(debounceTerm.toLowerCase()))]);
     }
 
     loadData();
@@ -27,14 +29,30 @@ export default function DraftProvider(props) {
   }, [debounceTerm]);
 
 
+  useEffect(() => {
+    // storeData.push(updatedPlayer);
+    // setSelectedPlayer(storeData)
+
+    storeData.splice(0, 1, updatedPlayer)
+    
+  }, [updatedPlayer])
+
+
   function onClick(e, player) {
-    storeData.push(player)
+    // storeData.push(player)
 
-    console.log(storeData);
+    // console.log(storeData);
 
-    setSelectedPlayer([...selectedPlayer, player]);
+    // setSelectedPlayer([...selectedPlayer, player]);
+    // selectedPlayer.splice(0, 1, player);
+    setUpdatedPlayer(player)
     setSearchTerm("");
     setSearchResults([]);
+  }
+
+
+  function changeDraftOrder(e, player) {
+
   }
 
 
@@ -42,6 +60,8 @@ export default function DraftProvider(props) {
     searchTerm, setSearchTerm,
     searchResults, setSearchResults,
     selectedPlayer, setSelectedPlayer,
+    updatedPlayer, setUpdatedPlayer,
+    draftPosition, setDraftPosition,
     onClick
   }
 
