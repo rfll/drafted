59a753ff62644ccf732bbc6b8460459af70cadb9
Ryan <ryanwfortin@gmail.com>
2022-11-24 22:10:53 -0800

    import context

--- src/components/PlayerSearch.jsx
+++ src/components/PlayerSearch.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import { useContext } from 'react';
+import { draftContext } from '../providers/DraftProvider';
+
 
 export default function PlayerSearch(props) {
+  // const {setIndex} = useContext(draftContext);
+
 
   return (
-    <div className='drafted-player-container'>
-      <div className='selected-player-info' key={props.name}>
-        <div className="draft-position">{props.index}</div>
-        <div className="selected-player-text">
-        <div className='selected-player-name'>{props.name}</div> 
+    <div className='selected-player-info' key={props.name}>
+      <div className="draft-position">{props.index}</div>
+      <div className="selected-player-text">
+        <div className='selected-player-name'>{props.name}</div>
         <div className='selected-player-team'>{props.team}</div>
-        </div>
-        <img className='selected-player-image' src={props.image} alt='draftee'></img>
-        </div>
-    </div>
-  )
+      </div>
+      <img className='selected-player-image' src={props.image} alt='draftee'></img>
+    </div>)
 }
