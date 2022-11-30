97ec2e3b8f651023421c9a77d78a494ba3748d3e
Ryan <ryanwfortin@gmail.com>
2022-11-30 00:40:12 -0800

    adjust condition for object syntax

--- src/components/DraftForm.jsx
+++ src/components/DraftForm.jsx
@@ -1,31 +1,35 @@
 import { useContext } from 'react';
 import { draftContext } from '../providers/DraftProvider';
+import storeData from '../data/storeData';
 import './PlayerResults.scss';
 import PlayerSearch from './PlayerSearch';
 import PlayerSearchEmpty from './PlayerSearchEmpty';
 
 export default function DraftForm(props) {
-  const { selectedPlayer } = useContext(draftContext);
-  const listPlayers = props.storeData;
+  const { selectedPlayer, updatedPlayer, setIndex } = useContext(draftContext);
 
+  return Object.values(selectedPlayer).map((player, index) => {
 
+    // console.log('player', player);
+    // console.log('index', index);
+    // console.log(player.position)
 
-  return selectedPlayer.map((player, index) => {
-
-    if (!player) {
-      console.log('goodbye')
+    if (!player.name) {
+      // console.log('goodbye')
       return (
         <div className='draft-position-container' key={index}>
-          <PlayerSearchEmpty index={index + 1} />
+          <PlayerSearchEmpty position={player.position} />
         </div>)
     }
 
-    if (player) {
-      console.log('bonjour')
+    if (player.name) {
+      // console.log('bonjour')
       return (
         <div className='draft-position-container' key={index}>
           <PlayerSearch index={index + 1} {...player} />
         </div>)
     }
+
+
   })
 }
