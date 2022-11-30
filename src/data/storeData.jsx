4b1b830bd7dfab4c4a31d2e3fb93298af1fb24a5
Ryan <ryanwfortin@gmail.com>
2022-11-30 00:41:17 -0800

    add data structure

--- src/data/storeData.jsx
+++ src/data/storeData.jsx
@@ -1,5 +1,48 @@
-const storeData = [];
+const storeData = [
+  {
+    'position': '1'
+  },
+  {
+    'position': '2'
+  },
+  {
+    'position': '3'
+  },
+  {
+    'position': '4'
+  },
+  {
+    'position': '5'
+  },
+  {
+    'position': '6'
+  },
+  {
+    'position': '7'
+  },
+  {
+    'position': '8'
+  },
+  {
+    'position': '9'
+  },
+  {
+    'position': '10'
+  },
+  {
+    'position': '11'
+  },
+  {
+    'position': '12'
+  },
+  {
+    'position': '13'
+  },
+  {
+    'position': '14'
+  }
+];
 
-console.log(storeData)
+// console.log(storeData)
 
 export default storeData;
