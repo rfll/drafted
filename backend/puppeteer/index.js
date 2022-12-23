393cacf2e71af775bd9cf65bd022ad27b17b6d3f
Ryan <ryanwfortin@gmail.com>
2022-12-22 22:27:43 -0800

    update data

--- backend/puppeteer/index.js
+++ backend/puppeteer/index.js
@@ -1,40 +1,45 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({
-    // headless --> false opens the browser to watch the script execute
-    headless: false,
-    // ignoreHTTPSErrors allows navigation through http
-    'ignoreHTTPSErrors': true
-  });
+async function scraper() {
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.on('console', msg => console.log(msg.text()));
+  // await page.goto('https://www.sports-reference.com/cbb/players/jarace-walker-1.html', { waitUntil: 'domcontentloaded' });
+  await page.goto('https://www.sports-reference.com/cbb/players/cam-whitmore-1.html', { waitUntil: 'domcontentloaded' });
 
-  await page.goto('https://www.sports-reference.com/cbb/players/jarace-walker-1.html');
 
-  // Type into search box.
-  // await page.type('.devsite-search-field', 'Headless Chrome');
+  const data = await page.$$eval('#wrap', option => {
+    return option.map(row => {
+      return {
+        name: row.querySelector("#meta > div > h1 > span").textContent,
+        height: row.querySelector("#meta > div > p:nth-child(3) > span:nth-child(1)").textContent,
+        weight: row.querySelector("#meta > div > p:nth-child(3) > span:nth-child(2)").textContent,
+        team: row.querySelector("#meta > div > p:nth-child(7) > a").textContent,
+        gamesPlayed: row.querySelector(".right[data-stat='games']").textContent,
+        minutes: row.querySelector(".right[data-stat='mp_per_g']").textContent,
+        points: row.querySelector(".right[data-stat='pts_per_g']").textContent,
+        assists: row.querySelector(".right[data-stat='ast_per_g']").textContent,
+        rebounds: row.querySelector(".right[data-stat='trb_per_g']").textContent,
+        steals: row.querySelector(".right[data-stat='stl_per_g']").textContent,
+        blocks: row.querySelector(".right[data-stat='blk_per_g']").textContent,
+        turnovers: row.querySelector(".right[data-stat='tov_per_g']").textContent,
+        fouls: row.querySelector(".right[data-stat='pf_per_g']").textContent,
+        fgPercentage: row.querySelector(".right[data-stat='fg_pct']").textContent,
+        threePercentage: row.querySelector(".right[data-stat='fg3_pct']").textContent,
+        stlPercentage: row.querySelector(".right[data-stat='stl_pct']").textContent,
+        blkPercentage: row.querySelector(".right[data-stat='blk_pct']").textContent,
+        per: row.querySelector(".right[data-stat='per']").textContent,
+        oRating: row.querySelector(".right[data-stat='off_rtg']").textContent,
+        dRating: row.querySelector(".right[data-stat='def_rtg']").textContent,
+        wsPer40: row.querySelector(".right[data-stat='ws_per_40']").textContent,
+        bpm: row.querySelector(".right[data-stat='bpm']").textContent
+      }
+    })
+  })
 
-  // Wait for suggest overlay to appear and click "show all results".
-  const allResultsSelector = '.devsite-suggest-all-results';
-  await page.waitForSelector(allResultsSelector);
-  await page.click(allResultsSelector);
-
-  // Wait for the page to load and display the results.
-  const resultsSelector = 'div > #div_players_per_game';
-  await page.waitForSelector(resultsSelector);
-
-  // Extract the results from the page.
-  const links = await page.evaluate(resultsSelector => {
-    return [...document.querySelectorAll(resultsSelector)].map(anchor => {
-      // const title = anchor.textContent.split('|')[0].trim();
-      // return `${title} - ${anchor.href}`;
-      console.log(anchor)
-    });
-  }, resultsSelector);
-
-  // Print all the files.
-  // console.log(links.join('\n'));
+  console.log(data);
 
   await browser.close();
-})();
+}
+
+
+scraper();
