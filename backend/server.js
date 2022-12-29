b349aab317232343872ae8c1a274a350e1a3329b
Ryan <ryanwfortin@gmail.com>
2022-12-28 22:07:25 -0800

    prepare db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
+const express = require('express');
+const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv').config();
-// dotenv.config();
-const uri = `${process.env.DB_URI}`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+app.use(express.static('../frontend'));
+app.listen(8080, console.log('Port 8080'));
+
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+(async() => {
+
+  const uri = `${process.env.DB_URI}`;
+  const client = new MongoClient(uri);
+  await client.connect();
+  await listDatabases(client);
+  await client.close();
+
+})();
