1b97391eaf2d50d24deed9137f7ac9bda8252792
Ryan <ryanwfortin@gmail.com>
2023-01-06 15:49:10 -0800

    fix react dnd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import DraftProvider from './providers/DraftProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
   <DraftProvider>
     <App />
   </DraftProvider>
