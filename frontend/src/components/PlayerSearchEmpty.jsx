78506274709dfb14ae031d0e11f0f9dd9d4f847b
Ryan <ryanwfortin@gmail.com>
2023-01-18 20:51:40 -0800

    add conditional classname

--- frontend/src/components/PlayerSearchEmpty.jsx
+++ frontend/src/components/PlayerSearchEmpty.jsx
@@ -3,15 +3,15 @@ import { Draggable } from "react-beautiful-dnd";
 import { draftContext } from "../providers/DraftProvider";
 
 export default function PlayerSearchEmpty(props) {
-  const { clickDraftSlot } = useContext(draftContext)
+  const { clickDraftSlot, index } = useContext(draftContext)
 
   return (
     <Draggable draggableId={props.index.toString()} index={props.index} key={props.index}>
       {(provided) => (
     <div 
-    className='selected-player-empty' 
+    className={index !== props.index ? 'selected-player-empty' : 'selected-player-clicked'}
     key={props.position}
-    onClick={e => clickDraftSlot(e.target, props)}
+    onClick={e => clickDraftSlot(e, props)}
     {...provided.draggableProps}
     {...provided.dragHandleProps}
     ref={provided.innerRef}>
