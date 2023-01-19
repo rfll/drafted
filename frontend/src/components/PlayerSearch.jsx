583bc0269bc32b7b3626de53fd305b63a3fb5954
Ryan <ryanwfortin@gmail.com>
2023-01-18 20:51:31 -0800

    add conditional classname

--- frontend/src/components/PlayerSearch.jsx
+++ frontend/src/components/PlayerSearch.jsx
@@ -1,27 +1,41 @@
-import { useContext } from 'react';
-import { draftContext } from '../providers/DraftProvider';
-import { Draggable } from 'react-beautiful-dnd';
-
+import { useContext } from "react";
+import { draftContext } from "../providers/DraftProvider";
+import { Draggable } from "react-beautiful-dnd";
 
 export default function PlayerSearch(props) {
+  const { clickDraftSlot, index } = useContext(draftContext)
 
   return (
-    <Draggable draggableId={props.index.toString()} index={props.index} key={props.index}>
+    <Draggable
+      draggableId={props.index.toString()}
+      index={props.index}
+      key={props.index}
+    >
       {(provided) => (
         <div
-          className='selected-player-info'
+          className={
+            index !== props.index
+              ? "selected-player-info"
+              : "selected-player-info-clicked"
+          }
           key={props.name}
+          onClick={e => clickDraftSlot(e, props)}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          ref={provided.innerRef}>
+          ref={provided.innerRef}
+        >
           <div className="draft-position">{props.position}</div>
           <div className="selected-player-text">
-            <div className='selected-player-name'>{props.name}</div>
-            <div className='selected-player-team'>{props.team}</div>
+            <div className="selected-player-name">{props.name}</div>
+            <div className="selected-player-team">{props.team}</div>
           </div>
-          <img className='selected-player-image' src={props.img} alt='draftee'></img>
+          <img
+            className="selected-player-image"
+            src={props.img}
+            alt="draftee"
+          ></img>
         </div>
       )}
     </Draggable>
-  )
+  );
 }
