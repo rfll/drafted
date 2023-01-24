ed5b1dca16c1c77181392d3094726b3c72e7ed5d
Ryan <ryanwfortin@gmail.com>
2023-01-23 22:19:01 -0800

    add div for components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
 // import fakeData from './data/fakeDataArray';
-import LiveSearch from './components/LiveSearch';
-import DraftForm from './components/DraftForm';
+import LiveSearch from "./components/LiveSearch";
+import DraftForm from "./components/DraftForm";
 // import storeData from './data/storeData';
-import DraftProvider, { draftContext } from './providers/DraftProvider';
+import DraftProvider, { draftContext } from "./providers/DraftProvider";
 // import storeDataObject from './data/storeDataObject';
-import { DragDropContext } from 'react-beautiful-dnd';
-import { useContext } from 'react';
+import { DragDropContext } from "react-beautiful-dnd";
+import { useContext } from "react";
 
 function App() {
   const { onDragEnd } = useContext(draftContext);
 
   return (
-    <DragDropContext
-      onDragEnd={onDragEnd}>
-      {/* <div className='parent-container'> */}
-        {/* <div>Mocks</div> */}
+    <DragDropContext onDragEnd={onDragEnd}>
+       <header className="logo">
+        hoopScout
+      </header>
+      <div className="parent-container">
         <LiveSearch />
         <DraftForm />
-      {/* </div> */}
+      </div>
     </DragDropContext>
   );
 }
