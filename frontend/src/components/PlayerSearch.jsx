d41de4c8ac35ad3e44954a9e8d2e3df991d3489a
Ryan <ryanwfortin@gmail.com>
2023-01-28 14:59:16 -0800

    add stats drop down

--- frontend/src/components/PlayerSearch.jsx
+++ frontend/src/components/PlayerSearch.jsx
@@ -3,7 +3,9 @@ import { draftContext } from "../providers/DraftProvider";
 import { Draggable } from "react-beautiful-dnd";
 
 export default function PlayerSearch(props) {
-  const { clickDraftSlot, index } = useContext(draftContext)
+  const { clickDraftSlot, index } = useContext(draftContext);
+
+  console.log(props);
 
   return (
     <Draggable
@@ -19,21 +21,53 @@ export default function PlayerSearch(props) {
               : "selected-player-info-clicked"
           }
           key={props.name}
-          onClick={e => clickDraftSlot(e, props)}
+          onClick={(e) => clickDraftSlot(e, props)}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
-          <div className="draft-position">{props.position}</div>
-          <div className="selected-player-text">
-            <div className="selected-player-name">{props.name}</div>
-            <div className="selected-player-team">{props.team}</div>
+          <div className="selected-player-info-top-row">
+            <div className="draft-position">{props.position}</div>
+            <div className="selected-player-text">
+              <div className="selected-player-name">{props.name}</div>
+              <div className="selected-player-team">{props.team}</div>
+            </div>
+            <img
+              className="selected-player-image"
+              src={props.img}
+              alt={props.name}
+            ></img>
           </div>
-          <img
-            className="selected-player-image"
-            src={props.img}
-            alt="draftee"
-          ></img>
+          {index === props.index && (
+            <table className="selected-player-info-stats">
+              <tr>
+                <th>Games Played</th>
+                <th>Points</th>
+                <th>Assists</th>
+                <th>Rebounds</th>
+                <th>Steals</th>
+                <th>Blocks</th>
+              </tr>
+              <tr>
+                <th>{props.gamesPlayed}</th>
+                <th>{props.pointsPerGame}</th>
+                <th>{props.assistsPerGame}</th>
+                <th>{props.reboundsPerGame}</th>
+                <th>{props.stealPerGame}</th>
+                <th>{props.blocksPerGame}</th>
+              </tr>
+              <tr>
+                <th>BPM</th>
+                <th>Steal %</th>
+                <th>Block %</th>
+              </tr>
+              <tr>
+                <th>{props.bpm}</th>
+                <th>{props.stlPercentage}</th>
+                <th>{props.blkPercentage}</th>
+              </tr>
+            </table>
+          )}
         </div>
       )}
     </Draggable>
