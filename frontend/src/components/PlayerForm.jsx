787069c457b2b64ebf0ad2a21bbe5d8e38cd3b7b
Ryan <ryanwfortin@gmail.com>
2023-01-27 17:20:47 -0800

    remove image tag

--- frontend/src/components/PlayerForm.jsx
+++ frontend/src/components/PlayerForm.jsx
@@ -8,7 +8,6 @@ export default function PlayerForm(props) {
     <div className='draft-results' onClick={e => onClick(e.target, props)}>
       <img className='player-image' src={props.img} alt='draftee'></img>
       <div className='player-info' key={props.name}>
-        {/* <img className='player-image' src={props.image} alt='draftee'></img>  */}
         <div className='player-name'>{props.name}</div> 
         <div className='player-team'>{props.team}</div>
         </div>
