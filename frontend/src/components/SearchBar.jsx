2655ba60cf8c2b276611050dd31e909f384ac9c4
Ryan <ryanwfortin@gmail.com>
2023-01-27 17:19:43 -0800

    add search icon

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -5,18 +5,31 @@ export default function SearchBar(props) {
   const { searchTerm, setSearchTerm, index } = useContext(draftContext);
 
   return (
-    // <section className="search">
-      <form className="search_form" onSubmit={event => event.preventDefault()}>
-        <input
-          className="radius"
-          spellCheck="false"
-          placeholder={`Search Players (${index + 1})`}
-          name="search"
-          type="text"
-          value={searchTerm}
-          onChange={event => setSearchTerm(event.target.value)}
-        />
-      </form>
-    // {/* </section> */}
+    <form className="search_form" onSubmit={(event) => event.preventDefault()}>
+      <input
+        className="radius"
+        spellCheck="false"
+        placeholder={`Search Players`}
+        name="search"
+        type="text"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+       <svg
+        className="search-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        // stroke="#000000"
+        // strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+      </svg>
+    </form>
   );
 }
