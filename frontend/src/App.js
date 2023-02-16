ae944969d76d597357ece2b688167d717abb5099
Ryan <ryanwfortin@gmail.com>
2023-02-15 22:51:46 -0800

    implement react-share

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import LiveSearch from "./components/LiveSearch";
 import DraftForm from "./components/DraftForm";
-import DraftProvider, { draftContext } from "./providers/DraftProvider";
+import { draftContext } from "./providers/DraftProvider";
 import { DragDropContext } from "react-beautiful-dnd";
+import {
+  TwitterIcon,
+  TwitterShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  RedditIcon,
+  RedditShareButton,
+} from "react-share";
 import { useContext } from "react";
-import './App.scss';
+import "./App.scss";
 
 function App() {
   const { onDragEnd } = useContext(draftContext);
@@ -13,6 +21,29 @@ function App() {
       <div className="parent-container">
         <LiveSearch />
         <DraftForm />
+        <div className="share-buttons">
+          <TwitterShareButton
+            title={"Drafted Big Board"}
+            url={window.location.href}
+            className="twitter-share"
+          >
+            <TwitterIcon size={42} borderRadius={18} />
+          </TwitterShareButton>
+          <FacebookShareButton
+            title={"Drafted Big Board"}
+            url={window.location.href}
+            className="twitter-share"
+          >
+            <FacebookIcon size={42} borderRadius={18} />
+          </FacebookShareButton>
+          <RedditShareButton
+            title={"Drafted Big Board"}
+            url={window.location.href}
+            className="twitter-share"
+          >
+            <RedditIcon size={42} borderRadius={18} />
+          </RedditShareButton>
+        </div>
       </div>
     </DragDropContext>
   );
