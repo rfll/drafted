7758615f6a7312330c7a651db7b343fdf878e537
Ryan <ryanwfortin@gmail.com>
2023-04-23 18:31:22 -0700

    format

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
-import App from './App';
-import DraftProvider from './providers/DraftProvider';
+import App from "./App";
+import DraftProvider from "./providers/DraftProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <DraftProvider>
-    <App />
-  </DraftProvider>
-   </React.StrictMode>
+    <DraftProvider>
+      <App />
+    </DraftProvider>
+  </React.StrictMode>
 );
