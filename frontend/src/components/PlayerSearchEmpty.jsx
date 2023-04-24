35262dc2b3ee59acb5b4ec214796887e519d6de5
Ryan <ryanwfortin@gmail.com>
2023-04-23 18:32:29 -0700

    add formatting

--- frontend/src/components/PlayerSearchEmpty.jsx
+++ frontend/src/components/PlayerSearchEmpty.jsx
@@ -3,19 +3,30 @@ import { Draggable } from "react-beautiful-dnd";
 import { draftContext } from "../providers/DraftProvider";
 
 export default function PlayerSearchEmpty(props) {
-  const { clickDraftSlot, index } = useContext(draftContext)
+  const { clickDraftSlot, index } = useContext(draftContext);
 
   return (
-    <Draggable draggableId={props.index.toString()} index={props.index} key={props.index}>
+    <Draggable
+      draggableId={props.index.toString()}
+      index={props.index}
+      key={props.index}
+    >
       {(provided) => (
-    <div 
-    className={index !== props.index ? 'selected-player-empty' : 'selected-player-clicked'}
-    key={props.position}
-    onClick={e => clickDraftSlot(e, props)}
-    {...provided.draggableProps}
-    {...provided.dragHandleProps}
-    ref={provided.innerRef}>
-      <div className="draft-position">{props.position}</div>
-    </div>)}
-    </Draggable>)
+        <div
+          className={
+            index !== props.index
+              ? "selected-player-empty"
+              : "selected-player-clicked"
+          }
+          key={props.position}
+          onClick={(e) => clickDraftSlot(e, props)}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <div className="draft-position">{props.position}</div>
+        </div>
+      )}
+    </Draggable>
+  );
 }
