2b6e6700ddaa3495065edcfac5c24fe6abae66a4
Ryan <ryanwfortin@gmail.com>
2023-05-02 20:06:45 -0700

    remove share features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import LiveSearch from "./components/LiveSearch";
 import DraftForm from "./components/DraftForm";
 import PlayerProfile from "./components/PlayerProfile";
+import Nav from "./components/Nav";
 import { draftContext } from "./providers/DraftProvider";
 import { DragDropContext } from "react-beautiful-dnd";
-import {
-  TwitterIcon,
-  TwitterShareButton,
-  FacebookIcon,
-  FacebookShareButton,
-  RedditIcon,
-  RedditShareButton,
-} from "react-share";
 import { useContext } from "react";
 import "./App.scss";
 
@@ -19,32 +12,13 @@ function App() {
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
+      <Nav />
       <div className="parent-container">
         <div className="left-column">
           <LiveSearch />
           <PlayerProfile />
         </div>
         <DraftForm />
-        <div className="buttons-social">
-          <TwitterShareButton
-            title={"Drafted Big Board"}
-            url={window.location.href}
-          >
-            <TwitterIcon size={42} borderRadius={18} />
-          </TwitterShareButton>
-          <FacebookShareButton
-            title={"Drafted Big Board"}
-            url={window.location.href}
-          >
-            <FacebookIcon size={42} borderRadius={18} />
-          </FacebookShareButton>
-          <RedditShareButton
-            title={"Drafted Big Board"}
-            url={window.location.href}
-          >
-            <RedditIcon size={42} borderRadius={18} />
-          </RedditShareButton>
-        </div>
       </div>
     </DragDropContext>
   );
