7748baa4f24e4f0222233accd4632c4ad2d4abdf
Ryan <ryanwfortin@gmail.com>
2023-06-04 13:37:13 -0700

    lint component

--- frontend/src/components/PlayerForm.jsx
+++ frontend/src/components/PlayerForm.jsx
@@ -1,16 +1,16 @@
-import { useContext } from 'react';
-import { draftContext } from '../providers/DraftProvider';
+import { useContext } from "react";
+import { draftContext } from "../providers/DraftProvider";
 
 export default function PlayerForm(props) {
   const { onClick } = useContext(draftContext);
 
   return (
-    <div className='draft-results' onClick={e => onClick(e.target, props)}>
-      <img className='player-image' src={props.img} alt='draftee'></img>
-      <div className='player-info' key={props.name}>
-        <div className='player-name'>{props.name}</div> 
-        <div className='player-team'>{props.team}</div>
-        </div>
+    <div className="draft-results" onClick={(e) => onClick(e.target, props)}>
+      <img className="player-image" src={props.img} alt="draftee"></img>
+      <div className="player-info" key={props.name}>
+        <div className="player-name">{props.name}</div>
+        <div className="player-team">{props.team}</div>
+      </div>
     </div>
-  )
+  );
 }
