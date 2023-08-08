a82014b7036158e3bebb7d12decda7da425886b1
Ryan <ryanwfortin@gmail.com>
2023-08-07 17:17:57 -0700

    change function and file name

--- frontend/src/components/SearchPlayerForm.jsx
+++ frontend/src/components/SearchPlayerForm.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { draftContext } from "../providers/DraftProvider";
 
-export default function PlayerForm(props) {
+export default function SearchPlayerForm(props) {
   const { onClick } = useContext(draftContext);
-
+  
   return (
-    <div className="draft-results" onClick={(e) => onClick(e.target, props)}>
-      <img className="player-image" src={props.img} alt="draftee"></img>
+    <div className="player-search-results" onClick={(e) => onClick(e.target, props)}>
+      <img className="player-image" src={props.img} alt={props.name}></img>
       <div className="player-info" key={props.name}>
         <div className="player-name">{props.name}</div>
         <div className="player-team">{props.team}</div>
