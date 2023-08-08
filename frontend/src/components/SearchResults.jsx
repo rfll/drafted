69347f4e569d9ae0f3a93349390210c0dbebd13d
Ryan <ryanwfortin@gmail.com>
2023-08-07 17:18:28 -0700

    change file name

--- frontend/src/components/SearchResults.jsx
+++ frontend/src/components/SearchResults.jsx
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { draftContext } from "../providers/DraftProvider";
-import PlayerForm from "./PlayerForm";
+import SearchPlayerForm from "./SearchPlayerForm";
 
 
 export default function SearchResults(props) {
   const { searchResults, onClick } = useContext(draftContext);
   
   return searchResults.map(player => {
-    return <PlayerForm key={player.name} {...player}
+    return <SearchPlayerForm key={player.name} {...player}
       onClick={onClick}
     />;
   });
